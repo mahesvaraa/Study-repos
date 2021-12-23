bc1a7c4ccfcd7142d279d094e4826110aba32502
Danila <danilashkirdov.itv@gmail.com>
2021-12-23 18:31:20 +0400

    Update

--- 1.3.py
+++ 1.3.py
@@ -1,34 +1,30 @@
-import requests
-import json
-res = []
+a = '''<cube color="blue">
+    <cube color="red">
+        <cube color="green">
+            <cube color="green">
+                <cube color="green">
+                    <cube color="blue">
+                    </cube>
+                    <cube color="green">
+                    </cube>
+                    <cube color="red">
+                    </cube>
+                </cube>
+            </cube>
+        </cube>
+    </cube>
+    <cube color="red">
+        <cube color="blue">
+        </cube>
+    </cube>
+</cube>'''
 
-client_id = 'bbc8b3c2dc1ec6f120be'
-client_secret = '95f5e8b95a285b47b6a54d01188a8b8e'
+from lxml import etree
 
-# инициируем запрос на получение токена
-r = requests.post("https://api.artsy.net/api/tokens/xapp_token",
-                  data={
-                      "client_id": client_id,
-                      "client_secret": client_secret
-                  })
-
-# разбираем ответ сервера
-j = json.loads(r.text)
-
-# достаем токен
-token = j["token"]
-
-# создаем заголовок, содержащий наш токен
-headers = {"X-Xapp-Token" : token}
-
-# инициируем запрос с заголовком
-for i in range(15):
-    line = input()
-    ln = r"https://api.artsy.net/api/artists/"+line.strip()+"/"
-    #print(ln)
-    r = requests.get(f"{ln}", headers=headers)
-    # разбираем ответ сервера
-    j = json.loads(r.text)
-    res.append((j['sortable_name'],j['birthday']))
-for i in sorted(res, key = lambda x: x[1]):
-    print(i[0])
+root = etree.fromstring(a)
+dic = {'red': 0, 'green': 0, 'blue': 0}
+tree = etree.ElementTree(root)
+for e in root.iter():
+    dic[e.attrib['color']] += tree.getpath(e).count('/')
+for i in dic.values():
+    print(i, end=' ')
