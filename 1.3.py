fae2a52761b8a11d3c99e7b42694db2d7690e33f
Danila <danilashkirdov.itv@gmail.com>
2021-12-23 19:00:25 +0400

    Update

--- 1.3.py
+++ 1.3.py
@@ -1,22 +1,10 @@
 a = '''<cube color="blue">
-    <cube color="red">
-        <cube color="green">
-            <cube color="green">
-                <cube color="green">
-                    <cube color="blue">
-                    </cube>
-                    <cube color="green">
-                    </cube>
-                    <cube color="red">
-                    </cube>
-                </cube>
-            </cube>
-        </cube>
+  <cube color="red">
+    <cube color="green">
     </cube>
-    <cube color="red">
-        <cube color="blue">
-        </cube>
-    </cube>
+  </cube>
+  <cube color="red">
+  </cube>
 </cube>'''
 
 from lxml import etree
@@ -25,6 +13,7 @@ root = etree.fromstring(a)
 dic = {'red': 0, 'green': 0, 'blue': 0}
 tree = etree.ElementTree(root)
 for e in root.iter():
+    print(e.attrib['color'], tree.getpath(e))
     dic[e.attrib['color']] += tree.getpath(e).count('/')
 for i in dic.values():
     print(i, end=' ')
