a8af7490e0dd08ca0c5517e0de8ee64f6c6315ca
Danila <danilashkirdov.itv@gmail.com>
2021-12-24 16:52:19 +0400

    Update

--- 1.3.2.py
+++ 1.3.2.py
@@ -1,4 +1,2 @@
-dollar, cent, pirog = [int(input()) for i in range(3)]
-itog_cent = (dollar * 100 + cent)
-ostatok = itog_cent * pirog
-print(ostatok // 100, ostatok % 100)
+a = int(input())
+print(-15 < a <= 12 or 14 < a < 17 or a > 19)
