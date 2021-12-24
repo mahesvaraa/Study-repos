11ac1b52f6cd20a87b870506df5ac8904c3caa27
Danila <danilashkirdov.itv@gmail.com>
2021-12-24 12:51:09 +0400

    Update

--- 1.3.2.py
+++ 1.3.2.py
@@ -1,7 +1,17 @@
-def closest_mod_5(x):
-    if x % 5 == 0:
-        return x
+text = input()
+
+temp = {}  # сколько раз встречается "тот или иной" символ
+for char in text:
+    if not char in temp:
+        temp.update({char: 1})
     else:
-        while x % 5 != 0:
-            x += 1
-        return x
+        temp[char] += 1
+
+# подсчет длины самого длинного палиндрома
+result = 0
+for key in temp:
+    result += 2 * (temp[key] // 2)
+if len(text) - result > 0:
+    result += 1
+
+print(result)
