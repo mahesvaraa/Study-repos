b6e39653fbb6beebfe426c70fb448ebfd54182cb
Danila <danilashkirdov.itv@gmail.com>
2022-01-10 18:59:27 +0400

    Update

--- 1.3.2.py
+++ 1.3.2.py
@@ -1,6 +1,2 @@
-p, dollars, cents = [int(input()) for i in range(3)]
-p = p / 100
-
-summ = dollars * 100 + cents
-print(int((summ * p + summ) // 100))
-print(int((summ * p + summ) % 100))
+word = input().lower()
+print(('no', 'yes')[word == word[::-1]])
