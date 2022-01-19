2c648ab75d6d8e2bc673e1cd2928de91529370d3
Danila <danilashkirdov.itv@gmail.com>
2022-01-19 11:38:58 +0400

    Update

--- 1.3.2.py
+++ 1.3.2.py
@@ -1,14 +1,101 @@
-from itertools import chain, combinations
-# функция проверяющая вообще все комбинации
+import colorama
+from colorama import Fore, Back, Style  # цветная консоль
+from matplotlib import pyplot as plt
+import time
+from functools import lru_cache
 
-def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
-    s = list(iterable)  # allows duplicate elements
-    return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
+colorama.init()
 
 
-stuff = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-var, result = [], []
-for i, combo in enumerate(powerset(stuff), 1):
-    if combo != ():
-        print('combo #{}: {}'.format(i, combo))
+def timed(f, *args, n_iter=100):
+    acc = float('inf')
+    for i in range(n_iter):
+        t0 = time.perf_counter()
+        f(*args)
+        t1 = time.perf_counter()
+        acc = min(acc, t1 - t0)
+    return acc
+
+
+print(Fore.RED + '# fib1 #' + Style.RESET_ALL)
+
+
+def fib1(n):
+    assert n >= 0
+    return n if n <= 1 else fib1(n - 1) + fib1(n - 2)
+
+
+print(fib1(8), '# time =', timed(fib1, 8))
+# print(fib1(80)) # долго
+cache = {}
+print(Fore.RED + '# fib 2 # ' + Style.RESET_ALL)
+
+
+def fib2(n):
+    assert n >= 0
+    if n not in cache:
+        cache[n] = n if n <= 1 else fib2(n - 1) + fib2(n - 2)
+    return cache[n]
+
+
+print(fib2(8), '# time =', timed(fib2, 8))
+print(fib2(80), '# time =', timed(fib2, 80))  # быстро
+
+
+def memo(f):
+    cache = {}
+
+    def inner(n):
+        if n not in cache:
+            cache[n] = f(n)
+        return cache[n]
+
+    return inner
+
+
+print(Fore.RED + '# memo fib1 #' + Style.RESET_ALL)
+
+
+@memo
+def fib1(x):
+    assert x >= 0
+    return x if x <= 1 else fib1(x - 1) + fib1(x - 2)
+
+
+print(fib1(8), '# time =', timed(fib1, 8))
+print(fib1(80), '# time =', timed(fib1, 80))
+print(Fore.RED + '# lru_cache fib1 #' + Style.RESET_ALL)
+
+
+@lru_cache(maxsize=None)
+def fib1(x):
+    assert x >= 0
+    return x if x <= 1 else fib1(x - 1) + fib1(x - 2)
+
+
+print(fib1(8), '# time =', timed(fib1, 8))
+print(fib1(80), '# time =', timed(fib1, 80))
+print(Fore.RED + '# fib3 #' + Style.RESET_ALL)
+
+
+def fib3(n):
+    assert n >= 0
+    f0, f1 = 0, 1
+    for i in range(n - 1):
+        f0, f1 = f1, f0 + f1
+    return f1
+
+
+print(fib3(8), '# time =', timed(fib3, 8))
+print(fib3(80), '# time =', timed(fib3, 80))
+
+
+def compare(fs, args):
+    for f in fs:
+        plt.plot(args, [timed(f, arg) for arg in args], label=f.__name__)
+    plt.show()
+    plt.legend()
+    plt.grid(True)
+
+
+compare([fib3], list(range(200)))
---
+++ selenium.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+import selenium.common.exceptions as exc
+import selenium.webdriver.support.expected_conditions as ec
+import time
+
+
+def is_element_appeared(driver, locator, timeout=10):
+    try:
+        WebDriverWait(driver,timeout,1,exc.TimeoutException).\
+            until(ec.presence_of_element_located(locator))
+    except exc.TimeoutException:
+        return False
+    return True
+
+def send_all_checkboxes(driver):
+    for i in driver.find_elements_by_css_selector('div.quiz-plugin span.s-checkbox__circle'):
+        i.click()
+    driver.find_element_by_css_selector('button.submit-submission').click()
+
+
+USERNAME = ''  # Your login (e-mail)
+PASSWORD = ''  # Your password
+
+browser = webdriver.Chrome()
+browser.implicitly_wait(10)
+wait = WebDriverWait(browser, 15)
+
+task_link = 'https://stepik.org/lesson/13230/step/8?unit=3416'
+
+# Authorization on Stepik
+browser.get('https://stepik.org/catalog?auth=login')
+elem = wait.until(ec.presence_of_element_located((By.ID, 'id_login_email')))
+elem.click()
+elem.send_keys(USERNAME)
+elem = browser.find_element_by_id('id_login_password')
+elem.click()
+elem.send_keys(PASSWORD)
+browser.find_element_by_css_selector('button.sign-form__btn').click()
+time.sleep(3)
+
+# Go get them, Tiger!
+browser.get(task_link)
+is_element_appeared(browser, (By.CSS_SELECTOR, 'div.attempt'))
+send_all_checkboxes(browser)
+tries_amount = 10
+while tries_amount and is_element_appeared(browser, (By.CSS_SELECTOR, 'span.attempt-message_wrong')):
+    browser.find_element_by_css_selector('button.success').click()
+    if is_element_appeared(browser,(By.CSS_SELECTOR, 'div.attempt-wrapper.no_submission.choice')):
+        send_all_checkboxes(browser)
+        tries_amount -= 1
+    else:
+        break
+
+browser.quit()
