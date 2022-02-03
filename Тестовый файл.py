8664886596c8be5e54d8d437e3c8ddea9d77d76a
Danila <danilashkirdov.itv@gmail.com>
2022-02-03 11:33:21 +0400

    Update

--- Тестовый файл.py
+++ Тестовый файл.py
@@ -1,80 +1,22 @@
-import random
+def number_to_words(num):
+    decimal = ["ноль", 'десять', "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят",
+               "девяносто"]
+    simple = ["ноль", 'один', "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"]
+    ten_to_twenty = ["десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать",
+                     "семнадцать", "восемнадцать", "девятнадцать"]
+    if num % 10 == 0:
+        return decimal[num // 10]
+    elif num < 10:
+        return simple[num]
+    elif 10 < num < 20:
+        return ten_to_twenty[num % 10]
+    elif num >= 20:
+        return decimal[num // 10] + " " + simple[num % 10]
+# считываем данные
+n = int(input())
 
-arr = [random.randint(-100, 100) for i in range(100)]
-arr2 = arr
+# вызываем функцию
+print(number_to_words(n))
+print(number_to_words(7))
+print(number_to_words(85))
 
-import time
-
-
-def selectionsort(arr):
-    for i in range(len(arr)):
-        k = i
-        for j in range(i + 1, len(arr)):
-            if arr[j] < arr[k]:
-                k = j
-        arr[i], arr[k] = arr[k], arr[i]
-    return arr
-
-
-def heapsort(arr):
-    h = arr.copy()
-    for i in range(len(arr)):
-        arr[i] = min(h)
-        h.remove(min(h))
-    return arr
-
-
-def countsort(arr):
-    from collections import Counter
-    res = []
-    c = list(Counter(arr).items())
-    # print(c)
-    for i, n in c:
-        res += [i] * n
-    return res
-
-
-def numericsort(arr=[267, 507, 912, 215, 109, 213, 199, 216, 257]):
-    arr = list(map(str, arr))
-    for i in reversed(range(len(str(arr[0])))):
-        arr.sort(key=lambda x: x[i])
-    return arr
-
-
-print(numericsort())
-
-
-# print(selectionsort(arr2))
-# print(heapsort(arr2))
-
-
-def timed(f, *args, n_iter=100):
-    acc = float('inf')
-    for i in range(n_iter):
-        t0 = time.perf_counter()
-        f(*args)
-        t1 = time.perf_counter()
-        acc = min(acc, t1 - t0)
-    return acc
-
-
-print(timed(heapsort, arr))
-print(timed(selectionsort, arr))
-print(timed(countsort, arr))
-
-
-def countsort(arr):
-    from collections import Counter
-    res = []
-    c = Counter(arr)
-
-    for j in range(11):
-        if c.get(j):
-            res += [j] * c.get(j)
-
-    return res
-
-
-n = input()
-arr = list(map(int, input().split()))
-print(*countsort(arr))
