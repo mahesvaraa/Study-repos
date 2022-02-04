00ea1fee87dada6b4303a14a2f731ca55e113b13
Danila <danilashkirdov.itv@gmail.com>
2022-02-04 11:40:13 +0400

    Update

--- Всякое/Тестовый файл.py
+++ Всякое/Тестовый файл.py
@@ -1,12 +1,14 @@
-from functools import reduce
-
-coefficients, x = input().split(), int(input())
-
-
-def evaluate(coefficients, x):
-    arr = [x for i in range(len(coefficients))]
-    step = [i for i in range(len(coefficients) - 1, -1, -1)]
-    print(reduce(lambda x, y: x + y, map(lambda x, y, n: int(x) * y ** n, coefficients, arr, step)))
-
-
-evaluate(coefficients, x)
+with open(input(), 'r', encoding='utf-8') as file, open('random.txt', 'w', encoding='utf-8') as output:
+    f, l = [], []
+    for line in file:
+        f.append(line.strip().replace(',', "").split())
+count_def = 0
+count_comment = 0
+for i in range(len(f)):
+    if 'def' in f[i]:
+        if f[i - 1] == [] or f[i - 1][0] != "#":
+            print(f[i][1].split("(")[0])
+            count_comment += 1
+        count_def += 1
+if count_def - count_comment == count_def:
+    print("Best Programming Team")
