9566862f74837c0ea3491d246ea4f646a6731cc4
Danila <danilashkirdov.itv@gmail.com>
2022-02-04 11:08:58 +0400

    Update

--- Тестовый файл.py
+++ Тестовый файл.py
@@ -1,22 +1,5 @@
-def number_to_words(num):
-    decimal = ["ноль", 'десять', "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят",
-               "девяносто"]
-    simple = ["ноль", 'один', "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"]
-    ten_to_twenty = ["десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать",
-                     "семнадцать", "восемнадцать", "девятнадцать"]
-    if num % 10 == 0:
-        return decimal[num // 10]
-    elif num < 10:
-        return simple[num]
-    elif 10 < num < 20:
-        return ten_to_twenty[num % 10]
-    elif num >= 20:
-        return decimal[num // 10] + " " + simple[num % 10]
-# считываем данные
+from fractions import Fraction as F
+
 n = int(input())
-
-# вызываем функцию
-print(number_to_words(n))
-print(number_to_words(7))
-print(number_to_words(85))
-
+x = sorted(list({F(i, j) for i in range(1, n + 1) for j in range(1, n + 1)}))
+[print(i) for i in x if i < 1]
