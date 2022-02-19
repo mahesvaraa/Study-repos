5507548518a94241cdc3e63d2cc1278e0ab4bcae
Danila <Danilashkirdow@mail.ru>
2022-02-19 19:41:44 +0400

    Updated Git

--- Automation-with-Selenium-and-Python/test_items.py
+++ Automation-with-Selenium-and-Python/test_items.py
@@ -7,12 +7,16 @@ def test_guest_should_see_login_link(browser, language):
     sleep(30)
     browser.find_element(By.CLASS_NAME, 'btn-add-to-basket').click()
     sleep(2)
+
     # проверяем наличие кнопки
     assert browser.find_element(By.XPATH, '//*[@id="messages"]/div[1]/div/strong')
+
     # дополнительно проверем ее работу
     result = browser.find_element(By.XPATH, '//*[@id="messages"]/div[1]/div/strong').text
     product_name = browser.find_element(By.XPATH, '//*[@id="content_inner"]/article/div[1]/div[2]/h1').text
+
     # сравниваем название <product_name> с сообщением после добавления "<result> добавлен в корзину"
     assert result == product_name
+
     # еще пишем в консоль о нашей проверке
     print(f'Проверка наличия кнопки "добавить в корзину" и ее нажатия на товаре {product_name}')
