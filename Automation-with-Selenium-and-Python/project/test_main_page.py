a1a795cc6f557384914f692cde4701a7334b1cc5
Danila <Danilashkirdow@mail.ru>
2022-02-20 11:43:37 +0400

    Create project

---
+++ Automation-with-Selenium-and-Python/project/test_main_page.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
