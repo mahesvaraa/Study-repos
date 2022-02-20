77b2e2ab7eed6c535c41ef8ee243fb2605352ade
Danila <Danilashkirdow@mail.ru>
2022-02-20 12:27:42 +0400

    Create methods for login_page

--- Automation-with-Selenium-and-Python/project/pages/main_page.py
+++ Automation-with-Selenium-and-Python/project/pages/main_page.py
@@ -7,3 +7,6 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
--- Automation-with-Selenium-and-Python/project/test_main_page.py
+++ Automation-with-Selenium-and-Python/project/test_main_page.py
@@ -1,8 +1,15 @@
 from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
+
+
+def go_to_login_page(browser):
+    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link.click()
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    page.should_be_login_link()
