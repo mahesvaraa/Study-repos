09441cf3476ad3595df6f0484c4546b3c345cb39
Danila <Danilashkirdow@mail.ru>
2022-09-12 12:22:11 +0400

    Revert "add course https://stepik.org/course/100707"
    This reverts commit ada7c6268305d96417ac844d8d72ac30c4468c6b.

--- Всякое/Тестовый файл.py
+++ Всякое/Тестовый файл.py
@@ -1,3 +1,11 @@
-s = input()
-s = s.replace(' ', '').replace('+', ' ').replace('-', ' -')
-print(sum(map(int, s.split())))
+class SoftList(list):
+
+    def __getitem__(self, item):
+        return super().__getitem__(item) if item in range(-len(self), len(self)) else False
+
+
+sl = SoftList("python")
+print(sl[0])  # 'p'
+print(sl[-1])  # 'n'
+print(sl[6])  # False
+print(sl[-7])  # False
