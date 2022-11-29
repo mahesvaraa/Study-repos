7003a96ab921b208fb5b875698103555c70a4674
Danila Shkirdov <danila.shkirdov@ru.axxonsoft.com>
2022-11-29 13:49:06 +0400

    Added new cource "Поколение Python": контесты по программированию

--- Всякое/Тестовый файл.py
+++ Всякое/Тестовый файл.py
@@ -1,5 +1,20 @@
-import re
-
-start, end = map(int, input().split())
-regex = re.compile(r'\d+')
-print(sum(map(int, regex.findall(input(), start, end))))
+simbols = """!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789"""
+a = input()
+c = a[::-1]
+b = a
+for i in simbols:
+    a = a.replace(i, '')
+if len(set(a)) == 1:
+    print(True)
+elif len(set(a[:len(a) // 2 + 1])) - len(set(a[len(a) // 2:])) not in [-1, 0, 1]:
+    print(False)
+else:
+    for k, v in enumerate(a):
+        if a[:k] + a[k + 1:] == (a[:k] + a[k + 1:])[::-1]:
+            print(True)
+            break
+        if c[:k] + c[k + 1:] == (c[:k] + c[k + 1:])[::-1]:
+            print(True)
+            break
+    else:
+        print(False)
